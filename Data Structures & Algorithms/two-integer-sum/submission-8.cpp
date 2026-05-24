cb5b3916c85ab1f81a2329b14a63667d86a5a59c
Ankit Sinha <87054411+Quantum-Codes@users.noreply.github.com>
2026-05-24 23:29:05 +0530

    Add: two-integer-sum - submission-8

---
+++ Data Structures & Algorithms/two-integer-sum/submission-8.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> x;
+        for (int i = 0; i < nums.size(); i++) {
+            int item = nums[i];
+            if (x.find(target - item) != x.end()) {
+                return {x[target - item], i};
+            }
+            x[item] = i;
+        }
+    }
+};
