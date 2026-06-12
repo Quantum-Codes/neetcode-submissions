269dcc56076f8b7d34bfe2ddf290c6a940c9ed15
Ankit Sinha <87054411+Quantum-Codes@users.noreply.github.com>
2026-06-13 00:41:09 +0530

    Add: eating-bananas - submission-1

---
+++ Data Structures & Algorithms/eating-bananas/submission-1.py
@@ -0,0 +1,22 @@
+import math
+
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        hours = lambda arr, k: float('inf') if k == 0  else sum([math.ceil(item / k) for item in arr])
+
+        left = 0
+        right = 10e12 # maxlen(piles)*maxitem(piles)
+        hrs = 0
+        while left <= right:
+            k = (left + right) // 2
+            hrs = hours(piles, k)
+
+            if hrs > h:
+                left = k + 1
+            else:
+                right = k - 1
+        
+        return int(left)
+
+
+        # 0 1 2 (3) 4 5 6 7 8 9
