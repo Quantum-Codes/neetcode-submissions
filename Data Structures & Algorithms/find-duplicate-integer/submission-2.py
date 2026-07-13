661b143736f13cb1810f801e2f8d96d64b119d51
Ankit Sinha <87054411+Quantum-Codes@users.noreply.github.com>
2026-07-13 14:18:17 +0530

    Add: find-duplicate-integer - submission-2

---
+++ Data Structures & Algorithms/find-duplicate-integer/submission-2.py
@@ -0,0 +1,16 @@
+class Solution:
+    #modifies
+    def findDuplicate(self, nums):
+        for i in range(len(nums)):
+            # Use the absolute value because the number might have been 
+            # negated by a previous visit
+            index = abs(nums[i]) - 1
+            
+            # If the value at this index is negative, we've seen it before
+            if nums[index] < 0:
+                return abs(nums[i])
+            
+            # Otherwise, mark it as seen by negating it
+            nums[index] = -nums[index]
+        
+        return -1
