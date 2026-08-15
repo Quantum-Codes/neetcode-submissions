cf2e66dbcd7b5722f0f1748598540439738a666d
Ankit Sinha <87054411+Quantum-Codes@users.noreply.github.com>
2026-08-15 16:27:47 +0530

    Add: coin-change - submission-0

---
+++ Data Structures & Algorithms/coin-change/submission-0.py
@@ -0,0 +1,29 @@
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        # greedy? [2 3 10], 14 -> 3 coins kinda. greedy didnt work. lets say we backtrack at choosing 3 and go to choose 2. now its greedy+dfs.
+        # [1 3 4], 6 -> here even that doesnt work.
+        # bruteforce would be to just explore every coin 
+        # choosing path and get min depth (bfs until 0)
+        # DP would be ???. cant think. lemme draw bfs to see
+        # okay many branches seem the same.
+        # so subproblem is that given a amount x, find 
+        # minimum to get to that x.
+        # then dp[i] = min(dp[x] : x=i-c for all c)
+
+        dp = [0] * (amount+1) # O(amount*coins.len)? worse than bfs?
+        # dp[0] = 0
+        for i in range(1, amount+1):
+            intermediates = []
+            for c in coins:
+                if i-c < 0:
+                    continue
+                if dp[i-c] == -1:
+                    continue # deadend
+                intermediates.append(dp[i-c])
+
+            if not intermediates:
+                dp[i] = -1
+                continue
+            dp[i] = min(intermediates) + 1
+        
+        return dp[amount]
